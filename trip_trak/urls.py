e632ee7d115b359fb1f7942bd4fb2697c5ec176f
CJ Francisco <Drowzyking1126@gmail.com>
2024-08-05 21:30:08 +0800

    Implement models and templates for auth and trips

--- trip_trak/urls.py
+++ trip_trak/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import HomeView
+from .views import HomeView, TripCreateView, TripDetailView, trip_list
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
+    path('dashboard/', trip_list, name='trip-list'),
+    path('dashboard/trip/create/', TripCreateView.as_view(), name='trip-create'),
+    path('dashboard/trip/<int:pk>/', TripDetailView.as_view(), name='trip-detail'),
 ]
--- trip_trak/views.py
+++ trip_trak/views.py
@@ -1,6 +1,42 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, CreateView, DetailView
+from django.urls import reverse_lazy
+
+
+from .models import Trip, Note
 
 # Create your views here.
 class HomeView(TemplateView):
     template_name = 'trip_trak/index.html' 
+
+
+def trip_list(request):
+    trips = Trip.objects.filter(owner=request.user.id)
+    context = {
+        'trips': trips
+    }
+    return render(request, 'trip_trak/trip_list.html', context)
+
+
+class TripCreateView(CreateView):
+    model = Trip
+    success_url = reverse_lazy('trip-list')
+    fields = ['city', 'country', 'start_date', 'end_date']
+
+
+    def form_valid(self, form):
+        form.instance.owner = self.request.user
+        return super().form_valid(form)
+
+
+class TripDetailView(DetailView):
+    model = Trip 
+
+    # data stored on trip
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        trip = context['object']
+        notes = trip.notes.all()
+        context['notes'] = notes
+
+        return context
