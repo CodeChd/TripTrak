7fe13854447d397607696625f0d0742ef03924f1
CJ Francisco <Drowzyking1126@gmail.com>
2024-08-06 00:01:39 +0800

    implement notes and trip crud

--- trip_trak/views.py
+++ trip_trak/views.py
@@ -1,5 +1,6 @@
+from django.db.models.query import QuerySet
 from django.shortcuts import render
-from django.views.generic import TemplateView, CreateView, DetailView
+from django.views.generic import TemplateView, CreateView, DetailView, ListView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 
 
@@ -40,3 +41,62 @@ class TripDetailView(DetailView):
         context['notes'] = notes
 
         return context
+
+
+class TripUpdateView(UpdateView):
+    model = Trip
+    success_url = reverse_lazy("trip-list")
+    fields = ['city', 'country', 'start_date', 'end_date']
+
+class TripDeleteView(DeleteView):
+    model = Trip
+    success_url = reverse_lazy("trip-list")
+
+
+
+
+
+
+class NoteDetailView(DetailView):
+    model = Note
+
+
+class NoteListView(ListView):
+    model = Note
+
+    # fetch only notes of the active user
+    def get_queryset(self):
+        queryset = Note.objects.filter(trip__owner=self.request.user) 
+        return queryset
+    
+
+class NoteCreateView(CreateView):
+    model = Note
+    success_url = reverse_lazy("note-list")
+    fields = "__all__"
+
+
+    def get_form(self):
+         form = super(NoteCreateView, self).get_form()
+         trips = Trip.objects.filter(owner=self.request.user)
+         form.fields['trip'].queryset = trips
+         return form
+
+
+
+class NoteUpdateView(UpdateView):
+    model = Note
+    success_url = reverse_lazy("note-list")
+    fields = "__all__"
+
+
+    def get_form(self):
+         form = super(NoteUpdateView, self).get_form()
+         trips = Trip.objects.filter(owner=self.request.user)
+         form.fields['trip'].queryset = trips
+         return form
+
+class NoteDeleteView(DeleteView):
+    model = Note
+    success_url = reverse_lazy("note-list")
+    
